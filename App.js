9a7a3468a5b9dc6827f8da10ad95c5de05369955
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2023-01-23 10:36:10 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-
+import { StyleSheet, Text, View, ScrollView } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import * as Location from "expo-location";
+import MapView, { Marker } from "react-native-maps";
 export default function App() {
   return (
     <View style={styles.titulo}>
