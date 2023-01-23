1f6ab1b97a65c340157fe25a062eaf8004fd6260
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2023-01-23 00:28:35 -0300

    titulo

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello, World!</Text>
+      <Text>App 2 - Marcação de Ponto</Text>
       <StatusBar style="auto" />
     </View>
   );
