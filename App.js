f3f37e0a070c4a31890ba485b1b37ff2e788ac03
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2023-01-23 10:33:48 -0300

    imports

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={styles.titulo}>
       <Text>App 2 - Marcação de Ponto</Text>
       <StatusBar style="auto" />
     </View>
@@ -13,8 +13,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
